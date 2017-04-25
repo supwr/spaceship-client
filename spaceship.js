911902dd7384b4e7a5c3677b32ee96bc11482ecb
Marcelo Rodrigues <marcelo.rodriguespires@gmail.com>
2017-04-25 00:28:53 -0300

    created fade method on click of chat button

--- spaceship.js
+++ spaceship.js
@@ -8,13 +8,15 @@
 
 		_Spaceship.spaceshipSettings = {};
 
+		_Spaceship.opacity = 0;
+
 		_Spaceship.conn = new WebSocket('ws://localhost:8888');
 
-    _Spaceship.conn.onopen = function(e) {
-        console.log("Connection established!");
-    };
+	    _Spaceship.conn.onopen = function(e) {
+	        console.log("Connection established!");
+	    };
 
-    _Spaceship.conn.onmessage = function(e) {
+	    _Spaceship.conn.onmessage = function(e) {
 
 			var sp__chat__box__holder = window.document.createElement("div");
 			sp__chat__box__holder.className = "sp__chat__box__holder";
@@ -28,13 +30,41 @@
 			chat__box__bubble.className = "chat__box__bubble";
 			chat__box__bubble.innerHTML = e.data;
 			window.document.getElementsByClassName("sp__chat__box__holder")[window.document.getElementsByClassName("sp__chat__box__holder").length - 1].appendChild(chat__box__bubble);
-      //$("#chat").append(e.data);
-    };
+	     
+	    };
+
+	    _Spaceship.fade = function (){	    	
+	    	if(_Spaceship.opacity == 0){
+
+	    		var intervalo = setInterval(function(){
+	    			if(_Spaceship.opacity < 100){
+	    				_Spaceship.opacity+=10;
+	    				window.document.getElementsByClassName("sp__ctn")[0].style.opacity = _Spaceship.opacity/100;
+	    				window.document.getElementsByClassName("sp__ctn")[0].style.display = "block";	    					    			
+	    			}else{
+	    				clearInterval(intervalo);
+	    			}
+	    		},50);	
+	    	}
+
+	    	if(_Spaceship.opacity == 100){
+
+	    		var intervalo = setInterval(function(){
+	    			if(_Spaceship.opacity > 0){
+	    				_Spaceship.opacity-=10;
+	    				window.document.getElementsByClassName("sp__ctn")[0].style.opacity = _Spaceship.opacity/100;	    					    				
+	    			}else{
+	    				window.document.getElementsByClassName("sp__ctn")[0].style.display = "none";
+    					clearInterval(intervalo);
+	    			}
+	    		},30);	
+	    	}
+	    }
 
 		_Spaceship.init = function(){
 			var css = window.document.createElement("style");
 			css.type = "text/css";
-			css.innerHTML = ".sp__ctn{ position: fixed;	right: 20px;bottom:90px;height: 70%;width:400px;border: 1px solid #CCC;border-radius: 10px;box-shadow: 0 7px 16px #CCC;font-family: Helvetica,Arial,sans-serif!important;} .sp__hdr{position: absolute;	height: 150px;top: 0;left:0;width: 100%;background-color: #2980b9;border-top-right-radius: 10px;border-top-left-radius: 10px;}.sp__chat{position: absolute;	top: 150px;	left:0;	padding: 10px 10px 10px 10px;bottom:60px;right: 0;overflow-y: scroll;}.sp__chat__box__holder{position:relative;float:left;width:100%;margin-bottom:15px}.sp__chat__box__avatar{position:relative;height:35px;float:left;background-color:#CCC;width:35px;border-radius:50%}.chat__box__bubble{position:relative;float:left;color:gray;margin-left:10px;background-color:#CCC;padding:5px 5px 5px 5px;word-wrap:break-word;min-height:30px;line-height:30px;width:80%;border-radius:5px}.sp__send__button{position:absolute;right:10px;top:6px;background-image:url('send.png');height:24px;width:24px;cursor:pointer}.sp__me__chat{float:left;background-color:#3498db;margin-left:45px;color:#FFF}.sp__txt{position: absolute;left: 0;bottom: 0;height:60px;width: 100%;border-bottom-right-radius: 10px;border-bottom-left-radius: 10px;}.sp__launcher{position: fixed;right: 20px;bottom:20px;height: 60px;width: 60px;border-radius: 50%;cursor: pointer;background-color: #2980b9;line-height: 70px;color: #FFF;text-align: center;box-shadow: 0 7px 16px #CCC;}.sp__inp__box{position: absolute;top:10px;bottom: 10px;right: 10px;left: 10px;border-radius: 5px;border: 1px solid #CCC;padding: 5px 45px 5px 5px;}.sp__txt__area{line-height: 20px;color:#666;border: none;width: 100%;height: 20px;resize: none;outline: none;}";
+			css.innerHTML = ".sp__ctn{ position: fixed;	right: 20px;bottom:90px;height: 70%;width:400px;border: 1px solid #CCC;border-radius: 10px;box-shadow: 0 7px 16px #CCC;font-family: Helvetica,Arial,sans-serif!important; font-size: 13px; display: none} .sp__hdr{position: absolute;	height: 150px;top: 0;left:0;width: 100%;background-color: #2980b9;border-top-right-radius: 10px;border-top-left-radius: 10px;}.sp__chat{position: absolute;	top: 150px;	left:0;	padding: 10px 10px 10px 10px;bottom:60px;right: 0;overflow-y: scroll;}.sp__chat__box__holder{position:relative;float:left;width:100%;margin-bottom:15px}.sp__chat__box__avatar{position:relative;height:35px;float:left;background-color:#CCC;width:35px;border-radius:50%}.chat__box__bubble{position:relative;float:left;color:gray;margin-left:10px;background-color:#CCC;padding:5px 5px 5px 5px;word-wrap:break-word;min-height:30px;line-height:30px;width:80%;border-radius:5px}.sp__send__button{position:absolute;right:10px;top:6px;background-image:url('send.png');height:24px;width:24px;cursor:pointer}.sp__me__chat{float:left;background-color:#3498db;margin-left:45px;color:#FFF}.sp__txt{position: absolute;left: 0;bottom: 0;height:60px;width: 100%;border-bottom-right-radius: 10px;border-bottom-left-radius: 10px;}.sp__launcher{position: fixed;right: 20px;bottom:20px;height: 60px;width: 60px;border-radius: 50%;cursor: pointer;background-color: #2980b9;line-height: 70px;color: #FFF;text-align: center;box-shadow: 0 7px 16px #CCC;}.sp__inp__box{position: absolute;top:10px;bottom: 10px;right: 10px;left: 10px;border-radius: 5px;border: 1px solid #CCC;padding: 5px 45px 5px 5px;}.sp__txt__area{line-height: 20px;color:#666;border: none;width: 100%;height: 20px;resize: none;outline: none;}";
 			window.document.head.appendChild(css);
 
 			var sp__ctn = window.document.createElement("div");
@@ -112,7 +142,13 @@
 					)
 				);
 				document.getElementsByClassName('sp__txt__area')[0].value = "";
-			}			
+			}
+
+			document.getElementsByClassName('sp__launcher')[0].onclick = function(e){
+				console.log("launcher");
+				_Spaceship.fade();
+			}
+							
 		}
 
 		_Spaceship.ahoy = function(){
